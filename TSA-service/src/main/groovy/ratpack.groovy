8be93b9e07ce0b07bbfe6d897d0b2ca19dffbf6b
Carolina Hadad <chadad@manas.com.ar>
2013-11-17 21:20:16 -0300

    Removed buid files and added gitignore :P

---
+++ TSA-service/src/main/groovy/ratpack.groovy
@@ -0,0 +1,39 @@
+import static org.ratpackframework.groovy.RatpackScript.ratpack
+import static org.ratpackframework.groovy.Template.groovyTemplate
+
+import org.bouncycastle.tsp.*
+
+
+ratpack {
+  handlers {
+
+    /*
+    Request:
+
+    TimeStampReq ::= SEQUENCE {
+      version               INTEGER { v1(1) },
+      messageImprint        MessageImprint,
+      --a hash algorithm OID and the hash value of the data to be time-stamped
+      reqPolicy            TSAPolicyId       OPTIONAL,
+      nonce                INTEGER         OPTIONAL,
+      certReq              BOOLEAN         DEFAULT FALSE,
+      extensions           [0] IMPLICIT Extensions OPTIONAL
+    }
+    */
+
+    get("get_timestamp") {
+
+      TimeStampRequestGenerator reqGen = new TimeStampRequestGenerator();
+
+      // Dummy request
+      TimeStampRequest request = reqGen.generate(
+        TSPAlgorithms.SHA1, new byte[20], BigInteger.valueOf(100));
+
+      byte[] reqData = request.getEncoded();
+
+
+      response.send "It worked ${reqData}"
+
+    }
+  }
+}
