a3ef8389afd3322c9093e1a7a6c4d79a344c1325
Gustavo Giráldez <ggiraldez@manas.com.ar>
2013-12-14 18:37:20 -0300

    Unit tests para Offerer

---
+++ Sinapuli-service/src/test/groovy/garantito/sinapuli/OffererTest.groovy
@@ -0,0 +1,15 @@
+package garantito.sinapuli
+
+import spock.lang.Specification
+
+class OffererTest extends Specification {
+  def "different offerers have different hash codes"() {
+    when:
+    def off1 = new Offerer("oferente 1")
+    def off2 = new Offerer("oferente 2")
+
+    then:
+    off1.hashCode() != off2.hashCode()
+  }
+}
+
